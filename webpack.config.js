16789d5d98caceb023f52687de1cc93ca3cbc5a3
Luca Park <qlife1146@gmail.com>
2023-01-10 09:52:14 +0900

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: "./src/",
     output: {
         path: path.resolve(__dirname, "/src/"),
-        filename: "[name].[chunkhash:8].jsc",
+        filename: "[name].[chunkhash:8].js",
     },
     module: {
         rules: [
